8f00d8c1f95fbb4ebcf18fe315f99aaab6089dab
Kushagra <kushagra.15554@gmail.com>
2023-11-01 14:50:01 +0530

    Markov chain

---
+++ Probability.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;  
+import java.io.FileReader;  
+import java.io.IOException; 
+public class Probability  
+{  
+public static void main(String[] args)   
+{
+String data[][]=new String[1220][];
+String line = "";  
+String splitBy = ",";
+int k=0;
+int h=0;
+int t=0;
+double hh=0;
+double tt=0;
+double ht=0;
+double th=0;
+try   
+{  
+//parsing a CSV file into BufferedReader class constructor  
+BufferedReader br = new BufferedReader(new FileReader("unbiased coins toss.csv"));
+while ((line = br.readLine()) != null)   //returns a Boolean value  
+{
+String[] toss = line.split(splitBy);    // use comma as separator  
+data[k]=toss;
+k++;
+}
+for(int i=0;i<1220;i++)
+{
+if(data[i][0].equals("H"))
+h++;
+else
+t++;
+}
+for(int i=0;i<1219;i++)
+{
+if(data[i][0].equals("H") && data[i+1][0].equals("H"))
+hh++;
+else if(data[i][0].equals("T") && data[i+1][0].equals("T"))
+tt++;
+else if(data[i][0].equals("H") && data[i+1][0].equals("T"))
+ht++;
+else if(data[i][0].equals("T") && data[i+1][0].equals("H"))
+th++;
+}
+hh=hh/h;
+tt=tt/t;
+ht=ht/t;
+th=th/h;
+System.out.println(hh+" "+tt+" "+ht+" "+th);
+br.close();
+}
+catch (IOException e)   
+{  
+e.printStackTrace();  
+}  
+}  
+}
---
+++ markov.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class markov {
+    public static void main(String[] args) {
+        int numTosses = 100;  // Number of coin tosses
+
+        // Transition matrix: [Heads, Tails]
+        double[][] transitionMatrix = {
+            //H     T
+            {0.47, 0.53},  // H
+            {0.54, 0.46}   // T
+        };
+
+        // Initial state: Start with "Heads"
+        int currentState = 0;  // 0 represents "Heads," 1 represents "Tails"
+
+        // Simulate coin tosses
+        Random random = new Random();
+        for (int toss = 1; toss <= numTosses; toss++) {
+            // Perform the next toss based on the transition probabilities
+            double randomValue = random.nextDouble();
+            if (randomValue < transitionMatrix[currentState][0]) {
+                // Transition to "Heads"
+                System.out.println("Toss " + toss + ": Heads");
+                currentState = 0;
+            } else {
+                // Transition to "Tails"
+                System.out.println("Toss " + toss + ": Tails");
+                currentState = 1;
+            }
+        }
+    }
+}
+
